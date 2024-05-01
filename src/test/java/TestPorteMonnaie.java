944a381b0cb1dca5afd4a09ff3484a9ec9fd7356
HelaHamza <helahamza2020@gmail.com>
2024-05-01 19:08:06 +0200

    fix project

--- src/test/java/TestPorteMonnaie.java
+++ src/test/java/TestPorteMonnaie.java
@@ -2,42 +2,43 @@ package test.java;
 
 import org.junit.jupiter.api.Test;
 
-import main.java.resources.PorteMonnaie;
 import main.java.resources.SommeArgent;
+import main.java.resources.PorteMonnaie;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestPorteMonnaie {
-	 @Test
-	    public void testAjouteSomme() {
-	        PorteMonnaie porteMonnaie1 = new PorteMonnaie();
-	        PorteMonnaie porteMonnaie2 = new PorteMonnaie();
+	
+    @Test
+    public void testAjouteSomme() {
+        PorteMonnaie porteMonnaie1 = new PorteMonnaie();
+        PorteMonnaie porteMonnaie2 = new PorteMonnaie();
 
-	        // Ajouter 5 euros à porteMonnaie1
-	        porteMonnaie1.ajouteSomme(new SommeArgent(5, "EUR"));
+        // Ajouter 5 euros à porteMonnaie1
+        porteMonnaie1.ajouteSomme(new SommeArgent(5, "EUR"));
 
-	        // Ajouter 5 autres euros à porteMonnaie1
-	        porteMonnaie1.ajouteSomme(new SommeArgent(5, "EUR"));
-	        
-	     // Ajouter 10 euros à porteMonnaie2
-	        porteMonnaie2.ajouteSomme(new SommeArgent(10, "EUR"));
+        // Ajouter 5 autres euros à porteMonnaie1
+        porteMonnaie1.ajouteSomme(new SommeArgent(5, "EUR"));
+        
+        // Ajouter 10 euros à porteMonnaie2
+        porteMonnaie2.ajouteSomme(new SommeArgent(10, "EUR"));
 
-	        // On s'attend à ce que porteMonnaie1 et porteMonnaie2 soient égaux
-	        assertTrue(porteMonnaie1.equals(porteMonnaie2));
-	    }
+        // On s'attend à ce que porteMonnaie1 et porteMonnaie2 soient égaux
+        assertTrue(porteMonnaie1.equals(porteMonnaie2));
+    }
 
-	    @Test
-	    public void testEquals() {
-	        PorteMonnaie porteMonnaie1 = new PorteMonnaie();
-	        PorteMonnaie porteMonnaie2 = new PorteMonnaie();
+    @Test
+    public void testEquals() {
+        PorteMonnaie porteMonnaie1 = new PorteMonnaie();
+        PorteMonnaie porteMonnaie2 = new PorteMonnaie();
 
-	        // Ajouter 5 euros à porteMonnaie1
-	        porteMonnaie1.ajouteSomme(new SommeArgent(5, "EUR"));
+        // Ajouter 5 euros à porteMonnaie1
+        porteMonnaie1.ajouteSomme(new SommeArgent(5, "EUR"));
 
-	        // Ajouter 7 dollars à porteMonnaie2
-	        porteMonnaie2.ajouteSomme(new SommeArgent(7, "USD"));
+        // Ajouter 7 dollars à porteMonnaie2
+        porteMonnaie2.ajouteSomme(new SommeArgent(7, "USD"));
 
-	        // On s'attend à ce que porteMonnaie1 et porteMonnaie2 soient différents
-	        assertFalse(porteMonnaie1.equals(porteMonnaie2));
-	    }
+        // On s'attend à ce que porteMonnaie1 et porteMonnaie2 soient différents
+        assertFalse(porteMonnaie1.equals(porteMonnaie2));
+    }
 }
