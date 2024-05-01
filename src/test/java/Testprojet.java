7531bab02346755a607388b216c926a1b6380a3e
HelaHamza <helahamza2020@gmail.com>
2024-05-01 21:08:21 +0200

    fix project 11

--- src/test/java/Testprojet.java
+++ src/test/java/Testprojet.java
@@ -1,63 +1,59 @@
 package test.java;
+
 import org.junit.jupiter.api.Test;
 
 import main.java.resources.SommeArgent;
+import main.java.resources.PorteMonnaie;
 import main.java.resources.UniteDistincteException;
 
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.AfterEach;
+
 import org.junit.jupiter.api.BeforeEach;
 
 public class Testprojet {
-    
-	private SommeArgent m12CHF;
+
+    private SommeArgent m12CHF;
     private SommeArgent m14CHF;
     private SommeArgent m14USD;
-	
-    private static int passage = 0;
-    
-	@BeforeEach
+
+    @BeforeEach
     public void setUp() {
-		m12CHF = new SommeArgent(12, "CHF");
+        m12CHF = new SommeArgent(12, "CHF");
         m14CHF = new SommeArgent(14, "CHF");
         m14USD = new SommeArgent(14, "USD");
-      //  passage++;
-        //System.out.println(passage + "ime passage avant exécution d'une méthode de test");
     }
-	//@AfterEach
-    //public void configAprés() {
-      //  System.out.println(passage + "ime passage APRES exécution d'une méthode de test");
-   // }
-	
-    @Test
-    public void testAddition() {
-    	try {
-            SommeArgent actualSum = m12CHF.add(m14CHF);
-            SommeArgent expectedSum = new SommeArgent(26, "CHF");
-            assertEquals(expectedSum, actualSum);
-        } catch (UniteDistincteException e) {
-            fail("L'addition a échoué avec l'exception: " + e.getMessage());
-        }
-  
-     }
-    
-    @Test
-    public void testAdditionUniteDistincteException() {
-            SommeArgent m12CHF = new SommeArgent(12, "CHF");
-            SommeArgent m14USD = new SommeArgent(14, "USD");
 
-            assertThrows(UniteDistincteException.class, () -> {
-                m12CHF.add(m14USD);
-            });
+  /*  @Test
+    public void testAdditionUniteDistincteException() {
+        assertThrows(UniteDistincteException.class, () -> {
+            m12CHF.add(m14USD);
+        });
      }
     
+*/
     @Test
     public void testEquals() {
-    
-        assertFalse(m12CHF.equals(null));
-        assertEquals(m12CHF, m12CHF);
-        assertEquals(m12CHF, new SommeArgent(12, "CHF"));
-        assertFalse(m12CHF.equals(m14CHF));
-        assertFalse(m14USD.equals(m14CHF));
+        // Création de deux porte-monnaie avec le même contenu
+        PorteMonnaie porteMonnaie1 = new PorteMonnaie();
+        PorteMonnaie porteMonnaie2 = new PorteMonnaie();
+        porteMonnaie1.ajouteSomme(new SommeArgent(10, "EUR"));
+        porteMonnaie1.ajouteSomme(new SommeArgent(20, "USD"));
+        porteMonnaie2.ajouteSomme(new SommeArgent(10, "EUR"));
+        porteMonnaie2.ajouteSomme(new SommeArgent(20, "USD"));
+
+        // On s'attend à ce que les deux porte-monnaie soient égaux
+        assertTrue(porteMonnaie1.equals(porteMonnaie2));
+
+        // Ajouter une somme supplémentaire à porteMonnaie1
+        porteMonnaie1.ajouteSomme(new SommeArgent(5, "EUR"));
+
+        // On s'attend à ce que les deux porte-monnaie soient maintenant différents
+        assertFalse(porteMonnaie1.equals(porteMonnaie2));
     }
+    
+    
+    
+    
+    
 }
+
